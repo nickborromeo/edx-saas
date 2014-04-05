99d16ce74a3c91c1c96a83d808823191a1c2ccf3
Nick Borromeo <ngborromeo@gmail.com>
2014-04-04 21:19:24 -0700

    brute force implementation of RPS

--- rock-paper-scissors/lib/rps.rb
+++ rock-paper-scissors/lib/rps.rb
@@ -4,6 +4,36 @@ class RPS
   def rps_game_winner(game)
     raise WrongNumberOfPlayersError unless game.length == 2
     raise NoSuchStrategyError unless valid_strategy?(game)
+
+    player1 = game[0]
+    player2 = game[1]
+
+    player1_strategy = game[0][1]
+    player2_strategy = game[1][1]
+
+    if player1_strategy == player2_strategy
+      return player1
+    elsif player1_strategy == "R"
+      if player2_strategy == "S"
+        return player1
+      else
+        return player2
+      end
+    elsif player2_strategy == "R"
+      if player1_strategy == "S"
+        return player2
+      else
+        return player1
+      end
+    elsif player1_strategy == "S"
+      if player2_strategy == "P"
+        return player1
+      end
+    elsif player2_strategy == "S"
+      if player1_strategy == "P"
+        return player2
+      end
+    end
   end
 
   private
--- rock-paper-scissors/spec/rps_spec.rb
+++ rock-paper-scissors/spec/rps_spec.rb
@@ -23,5 +23,42 @@ describe "RPS" do
       game = [ ["Armando", "P"], ["Dave", "S"] ]
       expect { rps.rps_game_winner(game)}.to_not raise_error
     end
+
+    it "should return first player when they use the same strategy" do
+      game = [ ["Armando", "R"], ["Dave", "R"] ]
+      rps.rps_game_winner(game).should == ["Armando", "R"]
+    end
+
+    it "R should win when given R and S" do
+      game = [ ["Armando", "R"], ["Dave", "S"] ]
+      rps.rps_game_winner(game).should == ["Armando", "R"]
+    end
+
+    it "R should win when given S and R" do
+      game = [ ["Armando", "S"], ["Dave", "R"] ]
+      rps.rps_game_winner(game).should == ["Dave", "R"]
+    end
+
+    it "P should win when given R and P" do
+      game = [ ["Armando", "R"], ["Dave", "P"] ]
+      rps.rps_game_winner(game).should == ["Dave", "P"]
+    end
+
+    it "P should win when given P and R" do
+      game = [ ["Armando", "P"], ["Dave", "R"] ]
+      rps.rps_game_winner(game).should == ["Armando", "P"]
+    end
+
+    it "S should win when given S and P" do
+      game = [ ["Armando", "S"], ["Dave", "P"] ]
+      rps.rps_game_winner(game).should == ["Armando", "S"]
+    end
+
+    it "S should win when given P and S" do
+      game = [ ["Armando", "P"], ["Dave", "S"] ]
+      rps.rps_game_winner(game).should == ["Dave", "S"]
+    end
+
   end
+
 end
