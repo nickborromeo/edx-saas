69d2d572ed58322747af58567092f2b9c6ab1086
Nick Borromeo <ngborromeo@gmail.com>
2014-04-10 21:11:52 -0700

    refactor is_palindrome to remove conditional

--- fun-with-strings/lib/string.rb
+++ fun-with-strings/lib/string.rb
@@ -3,11 +3,7 @@ class String
   def is_palindrome?
     word = self.downcase.gsub(/[^a-zA-z]/,'')
     inverted_word = word.reverse
-    if inverted_word != word
-      return false
-    else
-      return true
-    end
+    inverted_word == word
   end
 
   def count_words
